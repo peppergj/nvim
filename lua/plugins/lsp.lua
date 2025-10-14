8558dc67b4ab04ee4388a9991bdcc1ac6eaa2151
Pepper Gardner <pazzydawg@gmail.com>
2025-10-14 21:24:17 +1000

    added rust tools

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,39 +1,69 @@
 local ensure_installed = {
+	"rust_analyzer",
 	"lua_ls",
-	"pyright",
+	"tinymist",
+	"basedpyright",
 }
+
+local default_server_opts = {
+
+}
+
 return {
-	{ 
+	{
 		"williamboman/mason.nvim",
 		opts = {}
 	},
 	{
 		"neovim/nvim-lspconfig",
-		cmd = {"LspInfo", "LspInstall", "LspStart"},
-		event = {"BufReadPre", "BufNewFile"},
-		dependencies = {			
+		cmd = { "LspInfo", "LspInstall","LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
 			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim"
+			"williamboman/mason-lspconfig.nvim",
 		},
-
 		init = function()
 			vim.opt.signcolumn = "yes"
 		end,
-
-		config = function ()
+		config = function()
 			local lspconfig = require("lspconfig")
 
+			-- Stuff that only works when there is an active LSP
 			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "LSP actions", 
+				desc = "LSP actions",
 				callback = function(event)
 					local opts = { buffer = event.buf }
 
+					-- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
 					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+					-- MOVED TO SNACKS PICKER
+					-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+					vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = true}) end, opts)
+					vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 				end
 			})
-			require("mason-lspconfig").setup ({
+
+			require("mason-lspconfig").setup({
 				ensure_installed = ensure_installed,
+				automatic_enable = {
+					exclude = {
+						"rust_analyzer",
+					},
+				},
 			})
-		end 
+
+			require("mason-lspconfig").setup_handlers({
+			  function(server_name)
+				if server_name ~= "rust_analyzer" then
+				  lspconfig[server_name].setup(default_server_opts)
+				end
+			  end,
+			})
+		end
 	}
 }
---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,32 @@
+return {
+	"mrcjkb/rustaceanvim",
+	version = "^6",
+	lazy = false,
+	keys = {
+		{ "<leader>ca", function() vim.cmd.RustLsp("codeAction") end, ft = "rust" }
+	},
+	init = function()
+		vim.g.rustaceanvim = {
+			-- Plugin configuration
+			tools = {
+			},
+			-- LSP configuration
+			server = {
+				on_attach = function(client, bufnr)
+					-- you can also put keymaps in here
+				end,
+				default_settings = {
+					-- rust-analyzer language server configuration
+					['rust-analyzer'] = {
+						cargo = {
+							features = "all"
+						}
+					},
+				},
+			},
+			-- DAP configuration
+			dap = {
+			},
+		}
+	end
+}
